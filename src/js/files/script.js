a6565d01f9754b6d346120ba14420412f4d99041
Dima Lysak <60256445+z8Dimon8z@users.noreply.github.com>
2023-10-20 14:34:34 +0300

    Полностью сверстана шапка сайта

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,5 +3,15 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+// Подключение функционала табов не забыть установить node moduls npm install --save-dev mixitup
+
+import mixitup from 'mixitup';
+
+var mixer = mixitup('.main-tab__wraper', {
+    load: {
+        filter: '.category-b'
+    }
+});
+
 // Свой код пишем здесь
 
